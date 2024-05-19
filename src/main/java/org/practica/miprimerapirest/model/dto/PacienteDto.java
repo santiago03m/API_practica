ba9bee8775475649e6e4c89ed2e529c96490b32b
Santiago Rueda <santiruedadhhd@gmail.com>
2024-05-18 23:56:00 -0500

    plantillas y CRUD Paciente

--- src/main/java/org/practica/miprimerapirest/model/dto/PacienteDto.java
+++ src/main/java/org/practica/miprimerapirest/model/dto/PacienteDto.java
@@ -2,12 +2,16 @@ package org.practica.miprimerapirest.model.dto;
 
 import lombok.*;
 import org.practica.miprimerapirest.model.entity.Doctor;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @ToString
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PacienteDto implements Serializable {
     private Long pacienteId;
 
@@ -17,6 +21,7 @@ public class PacienteDto implements Serializable {
 
     private String tipoSangre;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date ultimaCita;
 
     private Doctor doctor;
